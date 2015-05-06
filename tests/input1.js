01f873e86cc2fa7cc1bacf4800da69cc287f98ef
Emiel Bruijntjes <emiel.bruijntjes@copernica.com>
2015-05-06 16:00:45 +0200

    extra features for tests

--- tests/input1.js
+++ tests/input1.js
@@ -1,8 +1,28 @@
 /**
  *  Input1.js
  */
+ 
+try
+{
+    var object = create("xyz");
+}
+catch (exception)
+{
+    throw 123;
+}
+ 
 alert('object.member: '+object.member);
 alert('object.method: '+object.method());
 alert('object.randomproperty: ' +object.randomproperty);
-alert('object.randommethod: ' +object.randommethod());
-alert('object(): '+object());
+//alert('object.randommethod: ' +object.randommethod());
+//alert('object(): '+object());
+
+var other = object.self();
+
+alert('other.member: '+other.member);
+
+// iterate over the properties
+//for (var x in object)
+//{
+//    alert('found property '+x);
+//}
