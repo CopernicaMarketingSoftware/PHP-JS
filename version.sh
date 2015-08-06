cbdd0a6e3de85ce7507542edca1fafa8817249b6
Emiel Bruijntjes <emiel.bruijntjes@copernica.com>
2015-08-06 11:47:12 +0200

    version number is now always set to latest tag

--- version.sh
+++ version.sh
@@ -4,14 +4,11 @@
 unset GREP_OPTIONS
 
 # do we have a project number or version directory?
-project=$(pwd -P | grep -o -e "[#][[:digit:]]\+")
-version=$(pwd -P | grep -o -e "PxJavascript/[[:digit:]]\+[.][[:digit:]]\+[.]\?[[:digit:]]\?")
+version=$(git tag | tail -n 1)
 
 # which one is set?
-if [ ! -z $project ]; then
-    echo "project $project"
-elif [ ! -z $version ]; then
-    echo $version | cut --delimiter=/ --fields=2
+if [ ! -z $version ]; then
+    echo $version | cut --delimiter=v --fields=2
 else
     echo "dev"
 fi
