06218e6a6dcfe5557c4ffe85985ddf32705dfd87
Emiel Bruijntjes <emiel.bruijntjes@copernica.com>
2026-07-02 16:53:37 +0200

    dead code

--- isolate.h
+++ isolate.h
@@ -11,7 +11,7 @@
  *  are properly thread-safe, this is an acceptable
  *  limitation
  *
- *  @copyright 2015 Copernica B.V.
+ *  @copyright 2015 - 2026 Copernica B.V.
  */
 
 /**
@@ -22,10 +22,7 @@
 /**
  *  Dependencies
  */
-//#include <v8-platform.h>
-//#include <chrono>
 #include <v8.h>
-//#include <map>
 
 /**
  *  Start namespace
@@ -61,18 +58,6 @@ private:
      */
     static const int ISOLATE_INDEX = 0;
     static const int CORE_INDEX = 1;
-
-
-    /**
-     *  List of tasks to execute
-     *  @var    std::multimap<std::chrono::system_clock::time_point, std::unique_ptr<v8::Task>>
-     */
-//    std::multimap<std::chrono::system_clock::time_point, std::unique_ptr<v8::Task>> _tasks;
-
-    /**
-     *  Perform all waiting tasks for this isolate
-     */
-//    void runTasks();
 
 public:
     /**
@@ -143,27 +128,6 @@ public:
         // is stored in a data field
         return static_cast<Core *>(isolate->GetData(CORE_INDEX));
     }
-
-    /**
-     *  Schedule a task to be executed
-     *
-     *  @param  isolate The isolate to schedule the task under
-     *  @param  task    The task to execute
-     *  @param  delay   Number of seconds to wait before executing
-     */
-    //static void scheduleTask(v8::Isolate *isolate, v8::Task *task, double delay);
-
-    /**
-     *  Get the isolate for this thread
-     *
-     *  @return The thread-local isolate instance
-     */
-    //static v8::Isolate *get();
-
-    /**
-     *  Clean up the isolate - if any - for this thread
-     */
-    //static void destroy();
 
     /**
      *  Cast to the underlying isolate
